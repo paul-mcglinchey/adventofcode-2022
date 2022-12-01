242fd61f2e802f4faef3096473a194ab563f6431
paul-mcglinchey <paulmcglinchey789@gmail.com>
2022-12-01 23:42:37 +0000

    Day 1 completed

---
+++ AdventOfCode2022.1/AdventOfCode2022.11/Program.cs
@@ -0,0 +1,25 @@
+﻿using (var reader = new StreamReader("./input.txt"))
+{
+    var line = reader.ReadLine();
+    var totalCals = new List<int>();
+    var current = 0;
+
+    while (line != null)
+    {
+        if (line.Trim() == string.Empty)
+        {
+            totalCals.Add(current);
+            current = 0;
+        } 
+        else
+        {
+            current += int.Parse(line.Trim());
+        }
+
+        line = reader.ReadLine();
+    }
+
+    Console.WriteLine($"Total cals of heftiest boi: {totalCals.Max()}");
+    Console.WriteLine($"Total cals of top 3 heftiest bois: {totalCals.OrderByDescending(c => c).Take(3).Sum()}");
+
+}
