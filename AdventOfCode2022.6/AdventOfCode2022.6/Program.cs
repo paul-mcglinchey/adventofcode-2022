5c8a91d92b8311ca112dd782b031823c39e543f9
paul-mcglinchey <paulmcglinchey789@gmail.com>
2022-12-06 10:32:54 +0000

    Cleaning up day 6 solution

--- AdventOfCode2022.6/AdventOfCode2022.6/Program.cs
+++ AdventOfCode2022.6/AdventOfCode2022.6/Program.cs
@@ -1,25 +1,19 @@
 ﻿var input = File.ReadAllText("./input.txt");
 
 var accumulator = "";
+var partOneFound = false;
 
 for (int i = 0; i < input.Length; i++)
 {
     accumulator = accumulator.Contains(input[i]) ? accumulator.Substring(accumulator.ToList().FindIndex(x => x == input[i]) + 1) + input[i] : accumulator + input[i];
 
-    if (accumulator.Length == 4)
+    if (accumulator.Length == 4 && !partOneFound)
     {
         Console.Write($"Part one: {accumulator} {i + 1}");
-        break;
+        partOneFound = true;
     }
-}
 
-accumulator = "";
-
-for (int i = 0; i < input.Length; i++)
-{
-    accumulator = accumulator.Contains(input[i]) ? accumulator.Substring(accumulator.ToList().FindIndex(x => x == input[i]) + 1) + input[i] : accumulator + input[i];
-
-    if (accumulator.Length == 14)
+    if(accumulator.Length == 14)
     {
         Console.Write($"Part two: {i + 1}");
         break;
