0bef2ccfc2a91c3d75beab66865a33b3f27b98e0
paul-mcglinchey <paulmcglinchey789@gmail.com>
2022-12-06 10:31:39 +0000

    Day 6 solution found

---
+++ AdventOfCode2022.6/AdventOfCode2022.6/Program.cs
@@ -0,0 +1,27 @@
+﻿var input = File.ReadAllText("./input.txt");
+
+var accumulator = "";
+
+for (int i = 0; i < input.Length; i++)
+{
+    accumulator = accumulator.Contains(input[i]) ? accumulator.Substring(accumulator.ToList().FindIndex(x => x == input[i]) + 1) + input[i] : accumulator + input[i];
+
+    if (accumulator.Length == 4)
+    {
+        Console.Write($"Part one: {accumulator} {i + 1}");
+        break;
+    }
+}
+
+accumulator = "";
+
+for (int i = 0; i < input.Length; i++)
+{
+    accumulator = accumulator.Contains(input[i]) ? accumulator.Substring(accumulator.ToList().FindIndex(x => x == input[i]) + 1) + input[i] : accumulator + input[i];
+
+    if (accumulator.Length == 14)
+    {
+        Console.Write($"Part two: {i + 1}");
+        break;
+    }
+}
