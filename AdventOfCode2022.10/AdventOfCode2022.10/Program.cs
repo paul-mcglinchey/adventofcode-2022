e715510c81cfd4b46e512b8bc6ca6d85bebc02ec
paul-mcglinchey <paulmcglinchey789@gmail.com>
2022-12-11 13:16:48 +0000

    Day 10 completed

---
+++ AdventOfCode2022.10/AdventOfCode2022.10/Program.cs
@@ -0,0 +1,65 @@
+﻿var input = File.ReadAllLines("./input.txt");
+
+var register = new List<int>() { 1 };
+var cycle = 0;
+var total = 0;
+var crtRow = "";
+var row = 0;
+
+foreach (var line in input)
+{
+    switch (line.Split(" ")[0])
+    {
+        case "addx":
+            var value = int.Parse(line.Split(" ")[1]);
+            UpdateTotal();
+            ScanForward();
+            UpdateTotal();
+            ScanForward();
+            register.Add(value);
+            break;
+        case "noop":
+            ScanForward();
+            UpdateTotal();
+            break;
+        default:
+            throw new NotImplementedException();
+    }
+}
+
+Console.WriteLine($"Part one: {total}");
+
+void UpdateTotal()
+{
+    if (cycle == 20 || ((cycle - 20) % 40 == 0))
+    {
+        var currentSum = register.Sum();
+        total += currentSum * cycle;
+    }
+}
+
+void ScanForward()
+{
+    if (cycle != 0 && cycle % 40 == 0)
+    {
+        Console.WriteLine(crtRow);
+        crtRow = "";
+
+        if (row == 6)
+        {
+            row = 0;
+            Console.WriteLine("\n");
+        }
+        else
+        {
+            row++;
+        }
+    }
+
+    var spriteCenterPosition = register.Sum();
+    var spriteRange = new HashSet<int> { spriteCenterPosition - 1, spriteCenterPosition, spriteCenterPosition + 1 };
+
+    crtRow += spriteRange.Contains(cycle - (row * 40)) ? '#' : '.';
+
+    cycle++;
+}
