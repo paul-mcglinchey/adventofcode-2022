5fef24a8f136d59f47e1146f755b414430834e92
paul-mcglinchey <paulmcglinchey789@gmail.com>
2022-12-12 09:57:15 +0000

    Scaffolded day 12

---
+++ AdventOfCode2022.12/AdventOfCode2022.12/Program.cs
@@ -0,0 +1,34 @@
+﻿var input = File.ReadAllLines("./input.txt");
+
+var visited = new HashSet<(int, int)>();
+(int, int)? end = null;
+(int, int)? start = null;
+
+for (int i = 0; i < input.Length; i++)
+{
+    var startIndex = input[i].IndexOf('S');
+    var endIndex = input[i].IndexOf('E');
+
+    if (startIndex != -1)
+    {
+        start = (i, startIndex);
+    }
+
+    if (endIndex != -1)
+    {
+        visited.Add((i, endIndex));
+        end = (i, endIndex);
+    };
+}
+
+while (!visited.Contains(start.Value))
+{
+    var position = visited.Last();
+
+    // capture all the surrounding moves and their respective heights
+
+    // eliminate movements
+
+    // weight the available moves if there's multiple
+}
+
